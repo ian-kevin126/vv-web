0ec948777f2d943f11bd3c0bf0497320a64c2d00
ian_kevin126 <ian_kevin126@126.com>
2021-07-27 17:36:37 +0800

    feat: add vvild-web branch

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,12 @@ function App() {
         <div>
             {/*<Login />*/}
             <WrapperCard>
-                <h1>父组件<Button type={'primary'} onClick={() => dispatch(SearchActions.openModal())}>打开弹框</Button></h1>
+                <div style={{display: "flex", alignItems: "center"}}>
+                    <div style={{fontSize: 30, fontWeight: "bolder"}}>父组件</div>
+                    <Button style={{marginLeft: 20}}
+                            type={'primary'}
+                            onClick={() => dispatch(SearchActions.openModal())}>打开弹框</Button>
+                </div>
                 <TaskList/>
                 <Search/>
             </WrapperCard>
@@ -23,10 +28,12 @@ function App() {
                    centered
                    closable
                    title={'我是弹框'}
+                   okText={'确定'}
+                   cancelText={'取消'}
                    onCancel={() => dispatch(SearchActions.closeModal())}
             >
                 <Content>
-                    我是弹框，你来打我了呀~
+                    我是弹框，你来打我呀11111~
                 </Content>
             </Modal>
         </div>
